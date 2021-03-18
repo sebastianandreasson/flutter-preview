aa03addd30c074e5aa0da14d66fd753720f1aa73
Sebastian Andreasson <xandse@ml-200403-001.local>
2021-03-18 09:13:56 +0100

    show fullscreen on mobile

--- app-preview/src/App.js
+++ app-preview/src/App.js
@@ -1,14 +1,17 @@
 import frame from './frame.svg'
 
 function App() {
+  const src = `${process.env.PUBLIC_URL}/app`
+
   return (
     <div className="App">
       <div className="phone">
         <div className="phone-inner">
-          <iframe title="App preview" src={`${process.env.PUBLIC_URL}/app`}></iframe>
+          <iframe className="desktop-iframe" title="App preview" src={src}></iframe>
         </div>
         <img alt="Phone frame" src={frame}></img>
       </div>
+      <iframe className="mobile-iframe" title="App preview" src={src}></iframe>
     </div>
   );
 }
